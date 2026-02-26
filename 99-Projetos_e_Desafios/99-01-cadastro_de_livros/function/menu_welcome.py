0bbfa282cb4d0e2ccdd62a7b294736da051d6ce1
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-02-26 16:11:55 -0300

    projeto - cadastrar livros - funcao iniciar

---
+++ 99-Projetos_e_Desafios/99-01-cadastro_de_livros/function/menu_welcome.py
@@ -0,0 +1,13 @@
+print("""
+
+Seja bem-vindo(a) ao cadastro de Livros
+      
+      Escolha uma das opções:
+
+      1- Adicionar livros;
+      2- Remover livros;
+
+      9- Sair do program;
+
+
+""")
